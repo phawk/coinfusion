ccb5b0e4556110567d309a6e9d2e449176107604
Pete Hawkins <petey@hey.com>
2022-04-03 20:37:53 +0100

    Add spec for has_secure_password

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Account, type: :model do
+  it { is_expected.to have_secure_password }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to allow_value("test@example.org").for(:email) }
