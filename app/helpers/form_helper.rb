6d346a26f1719ae7afc0a6212ceffcb0998e65f0
Pete Hawkins <petey@hey.com>
2022-04-08 12:51:09 +0100

    Start form styling

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,5 @@
+module FormHelper
+  def input_class
+    "placeholder:text-slate-400 text-slate-700 border-2 py-2.5 px-4 border-slate-200 bg-slate-50 rounded-xl w-full text-lg sm:text-xl focus:border-purple-400 focus:ring-purple-400"
+  end
+end
