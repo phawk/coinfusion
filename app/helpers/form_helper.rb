9d60ba49536de9b59f2951c818a032c14e046e2c
Pete Hawkins <petey@hey.com>
2022-04-08 13:02:24 +0100

    Style up the form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,4 +2,8 @@ module FormHelper
   def input_class
     "placeholder:text-slate-400 text-slate-700 border-2 py-2.5 px-4 border-slate-200 bg-slate-50 rounded-xl w-full text-lg sm:text-xl focus:border-purple-400 focus:ring-purple-400"
   end
+
+  def button_class
+    "bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-xl py-2.5 px-4 text-center text-lg sm:text-xl font-semibold w-full"
+  end
 end
