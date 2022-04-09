8e0de21362fd8daf30b5b094738cc02f6debb6a2
Pete Hawkins <petey@hey.com>
2022-04-09 10:44:36 +0100

    Tidy up account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,7 +7,7 @@ class Account < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, email: true, presence: true
-  validates :company_number, presence: true, if: :company?
-  validates :date_of_birth, presence: true, if: :individual?
   validates :password, length: { minimum: 8 }
+  validates :date_of_birth, presence: true, if: :individual?
+  validates :company_number, presence: true, if: :company?
 end
