1c51a42a3e78112b7dcb1a9f84de8860de9d1bd4
Pete Hawkins <petey@hey.com>
2022-04-22 19:26:25 +0100

    Update spec description

--- spec/graphql/mutations/register_spec.rb
+++ spec/graphql/mutations/register_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "mutation register" do
-  it "creates a new account"
+  it "creates a new account returning a token"
   it "returns an error when validation fails"
 end
